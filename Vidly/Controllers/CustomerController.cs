5988784540f4c90601c29c19aef1eece91dd93b0
Stef Delly <stefangayle@hotmail.co.uk>
2016-05-28 13:53:28 +0100

    Working with Data

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -9,28 +9,50 @@ namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
 
-        List<Customer> _customers = new List<Customer>
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        //DbContext is a disposable object => so have to properly dispose of it
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer 
+        private IEnumerable<Customer> GetCustomers() {
+            //Hardcoded getting customers
+            return  new List<Customer>
             {
                 new Customer {Id = 1, Name = "Tom Jones"},
                 new Customer {Id = 2, Name = "Sharleen Gayle"}
             };
+        }
 
         public ActionResult Index()
         {
-          
+            
+            //var _customers = GetCustomers().ToList();
+            //Get all customers from database
+            var customers = _context.Customers.ToList();
 
-            return View(_customers);
+            return View(customers);
            
         }
 
 
 
+
         public ActionResult CustomerDetails(int? id)
         {
-         var  details =  _customers.Find(x => x.Id == id);
-            if (details == null) throw new ArgumentNullException(nameof(details));
+            //var  details = GetCustomers().SingleOrDefault(x => x.Id == id);
+            var  details = _context.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (details == null) return HttpNotFound();
             var customer = details; 
             return View(customer);
         }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vidly.Models
 {
     public class Customer
-    {
+    {   //Primary Key
         public int Id { get; set; }
+        //Makes table not nullable (have to enter value)
+        [Required]
+        //data anotations
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //Naviagation Property -from one type to another
+        public MembershipType MembershipType { get; set; }
+        //foreignKey - dont want to load the whole membership object
+        public byte MembershipTypeId { get; set; }
+        
     }
 }
